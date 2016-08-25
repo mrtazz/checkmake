31d43be4c0e872f0c659e71d6591ca9d781b60fb
Daniel Schauenberg <d@unwiredcouch.com>
2016-08-25 11:06:16 -0400

    get basic variable parsing working
    this employs a simple regex based parser to parse out variables from
    Makefiles. Since rules are almost the same format, this also already parses
    out rule names and dependencies, however it doesn't account for rule bodies
    yet.

--- parser/parser.go
+++ parser/parser.go
@@ -1,8 +1,14 @@
-// Package parser implements all the parser functionality for Makefiles
+// Package parser implements all the parser functionality for Makefiles. This
+// is supposed to be a parser with a very small feature set that just supports
+// what is needed to do linting and checking and not actual full Makefile
+// parsing. And it's handrolled because apparently GNU Make doesn't have a
+// grammar (see http://www.mail-archive.com/help-make@gnu.org/msg02778.html)
 package parser
 
 import (
 	"errors"
+	"log"
+	"regexp"
 	"strings"
 )
 
@@ -32,8 +38,16 @@ type Variable struct {
 // VariableList represents a list of variables
 type VariableList []Variable
 
+var (
+	reFindRule             = regexp.MustCompile("^([a-zA-Z]+):(.*)")
+	reFindSimpleVariable   = regexp.MustCompile("^([a-zA-Z]+) ?:=(.*)")
+	reFindExpandedVariable = regexp.MustCompile("^([a-zA-Z]+) ?=(.*)")
+)
+
 // Parse is the main function to parse a Makefile from a file path string to a
-// Makefile struct
+// Makefile struct. This function should be kept fairly small and ideally most
+// of the heavy lifting will live in the specific parsing functions below that
+// know how to deal with individual lines.
 func Parse(filepath string) (ret Makefile, err error) {
 
 	var scanner *MakefileScanner
@@ -44,8 +58,6 @@ func Parse(filepath string) (ret Makefile, err error) {
 
 	for scanner.Scan() {
 		switch {
-		case strings.HasPrefix(scanner.Text(), "\t"):
-			// parse rule body here
 		case strings.HasPrefix(scanner.Text(), "#"):
 			// parse comments here, ignoring them for now
 			break
@@ -80,7 +92,30 @@ func Parse(filepath string) (ret Makefile, err error) {
 // subsequent lines if there is a rule body to parse and returns an interface
 // that is either a Variable or a Rule struct and leaves the scanner in a
 // state where it resides on the first line after the content parsed into the
-// returned struct.
+// returned struct. The parsing of line details is done via regexing for now
+// since it seems ok as a first pass but will likely have to change later into
+// a proper lexer/parser setup.
 func parseRuleOrVariable(scanner *MakefileScanner) (ret interface{}, err error) {
+	line := scanner.Text()
+
+	if matches := reFindRule.FindStringSubmatch(line); matches != nil {
+		log.Printf("found rule in '%s' with matches %v\n", line, matches)
+		ret = Rule{
+			Target:       matches[1],
+			Dependencies: strings.Split(matches[2], " ")}
+	} else if matches := reFindSimpleVariable.FindStringSubmatch(line); matches != nil {
+		ret = Variable{
+			Name:           matches[1],
+			Assignment:     matches[2],
+			SimplyExpanded: true}
+	} else if matches := reFindExpandedVariable.FindStringSubmatch(line); matches != nil {
+		ret = Variable{
+			Name:           matches[1],
+			Assignment:     matches[2],
+			SimplyExpanded: false}
+	} else {
+		log.Printf("didn't match '%s' with anything\n", line)
+	}
+
 	return
 }
