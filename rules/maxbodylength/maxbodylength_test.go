5baeea95b0ef4c22b3827b594dc50259216d53ee
Michael Adam <obnox@samba.org>
2025-05-08 19:20:07 +0200

    rules: expand the description of the maxbodylength rule
    The description of the maxbodylength rule shown with --list-rules
    weas not very specific:
    
      maxbodylength       Target bodies should be kept
                          simple and short.
    
    This change adds the detail that length refers to a certain maximum
    number of lines:
    
      maxbodylength       Target bodies should be kept
                          simple and short(no more than
                          5 lines).
    
    Signed-off-by: Michael Adam <obnox@samba.org>

--- rules/maxbodylength/maxbodylength_test.go
+++ rules/maxbodylength/maxbodylength_test.go
@@ -29,7 +29,7 @@ func TestFooIsTooLong(t *testing.T) {
 	ret := rule.Run(makefile, rules.RuleConfig{})
 
 	assert.Equal(t, 1, len(ret))
-	assert.Equal(t, "Target bodies should be kept simple and short.",
+	assert.Equal(t, "Target bodies should be kept simple and short (no more than 5 lines).",
 		rule.Description())
 	assert.Equal(t, "Target body for \"foo\" exceeds allowed length of 5 lines (7).", ret[0].Violation)
 	assert.Equal(t, 1, ret[0].LineNumber)
@@ -57,7 +57,7 @@ func TestFooIsTooLongWithConfig(t *testing.T) {
 	ret := rule.Run(makefile, cfg)
 
 	assert.Equal(t, 1, len(ret))
-	assert.Equal(t, "Target bodies should be kept simple and short.",
+	assert.Equal(t, "Target bodies should be kept simple and short (no more than 3 lines).",
 		rule.Description())
 	assert.Equal(t, "Target body for \"foo\" exceeds allowed length of 3 lines (4).", ret[0].Violation)
 	assert.Equal(t, 1, ret[0].LineNumber)
